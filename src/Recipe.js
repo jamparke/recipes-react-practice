b78ebb4fb3c68d972d094c56988d410c1ee3e918
Andy Parker <andyparker84@gmail.com>
2017-11-20 12:53:17 -0600

    Main Recipe card complete and styled

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
+import './Recipe.css'
 
 class Recipe extends Component {
     render() {
+        const { title, image, instructions } = this.props;
         const ingredients = this.props.ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient}</li>
         ))
         return (
-            <div>
-                Hello {this.props.title}
-                <ul>
-                    {ingredients}
-                </ul>
-            </div>
+            <div className='recipe-card'>
+                <div className='recipe-card-img'>
+                    <img src={image} alt={title} />
+                </div>
+                <div className='recipe-card-content'>
+                    <h3 className='recipe-card-title'>Recipe {this.props.title}</h3>
+                    <h4>Ingredients:</h4>
+                    <ul>
+                        {ingredients}
+                    </ul>
+                    <h4>Instructions:</h4>
+                    <p>{instructions}</p>
+                </div>
+            </div >
         )
     }
 }
