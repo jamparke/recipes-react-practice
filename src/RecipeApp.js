6fd73fc44a40478d05f3604359d7c91d0b7a2baf
Andy Parker <andyparker84@gmail.com>
2017-11-20 11:51:01 -0600

    first component create and linked

---
+++ src/RecipeApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Recipe from './Recipe.js'
+import './RecipeApp.css';
+
+
+class RecipeApp extends Component {
+  render() {
+    return (
+      <Recipe />
+    );
+  }
+}
+
+export default RecipeApp;
+
